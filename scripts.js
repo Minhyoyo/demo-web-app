e54bc298975f3f1787aa1a5a31930df8ef84dd87
Minhyoyo <nguyentrachminh@gmail.com>
2024-10-01 16:19:17 +0700

    Delete scripts.js

--- scripts.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('contact-form').addEventListener('submit', function(event) {
-    event.preventDefault();
-    alert('Thank you, ' + document.getElementById('name').value + '! Your message has been sent.');
-});
